89b298a9bdf4bfb4f392aec61ba7485e9425fed5
prithivraj-toast <prithivraj.selvaraj@toasttab.com>
2019-11-07 23:15:40 +0000

    Offline crash fixed

--- app/src/main/java/com/zestworks/livecurrency/currencylist/CurrencyListNetworkCurrencyListRepository.kt
+++ app/src/main/java/com/zestworks/livecurrency/currencylist/CurrencyListNetworkCurrencyListRepository.kt
@@ -1,26 +1,31 @@
 package com.zestworks.livecurrency.currencylist
 
 import com.zestworks.helpers.NetworkResult
+import java.io.IOException
 
 class CurrencyListNetworkCurrencyListRepository(private val currencyListApi: CurrencyListApi) :
     CurrencyListRepository {
     override suspend fun getLatestRates(baseCurrencyName: String): NetworkResult<CurrencyListResponse> {
-        val latestRates = currencyListApi.getLatestRates(base = baseCurrencyName)
-        return when (latestRates.isSuccessful) {
-            true -> {
-                val body = latestRates.body()
-                return when (body == null) {
-                    true -> {
-                        NetworkResult.Error("Error : Empty body.")
-                    }
-                    false -> {
-                        NetworkResult.Success(body)
+        try {
+            val latestRates = currencyListApi.getLatestRates(base = baseCurrencyName)
+            return when (latestRates.isSuccessful) {
+                true -> {
+                    val body = latestRates.body()
+                    return when (body == null) {
+                        true -> {
+                            NetworkResult.Error("Error : Empty body.")
+                        }
+                        false -> {
+                            NetworkResult.Success(body)
+                        }
                     }
                 }
+                false -> {
+                    NetworkResult.Error("Error : Network request failed.")
+                }
             }
-            false -> {
-                NetworkResult.Error("Error : Network request failed.")
-            }
+        } catch (retrofitError: IOException){
+            return NetworkResult.Error("Error : Network request failed.")
         }
     }
 }
